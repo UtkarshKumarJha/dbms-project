5b00445ea6a4b4729ba434a15a62eb99c49ebbcf
UtkarshKumarJha <utk.kumarjha@gmail.com>
2025-06-29 14:23:15 +0530

    fix: removed env from github

--- dbms-project/src/services/api.js
+++ dbms-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000", // Change to your backend URL if hosted
+    baseURL: "http://localhost:5000", 
     headers: {
         "Content-Type": "application/json",
     },
